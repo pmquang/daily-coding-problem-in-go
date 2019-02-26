2ca0efbac8c5fbcdab91542f2efab5be00f10ce9
khoi <khoiracle@gmail.com>
2019-02-26 12:46:38 +0700

    update algo

--- 6_1_stamp_collection.go
+++ 6_1_stamp_collection.go
@@ -2,22 +2,20 @@ package daily_coding_problem_in_go
 
 func Stamp(arr []int) []int {
 	m := make(map[int]int)
-	result := make([]int, len(arr))
+	var result []int
 
 	for _, e := range arr {
-		if _, ok := m[e]; ok {
-			m[e]++
+		if _, ok := m[e]; !ok {
+			m[e] = 1
 			continue
 		}
-		m[e] = 1
-	}
 
-	for k, v := range m {
-		if v > 2 {
-			for i := 0; i < v-2; i++ {
-				result = append(result, k)
-			}
+		if m[e] == 2 {
+			result = append(result, e)
+			continue
 		}
+
+		m[e]++
 	}
 
 	return result
--- 6_1_stamp_collection_test.go
+++ 6_1_stamp_collection_test.go
@@ -6,7 +6,8 @@ var stampTests = []struct {
 	in  []int
 	out []int
 }{
-	{[]int{1, 7, 3, 1, 7, 4, 5, 1, 7, 1}, []int{1, 1, 7}},
+	{[]int{1, 1, 1, 3}, []int{1}},
+	{[]int{1, 7, 3, 1, 7, 4, 5, 1, 7, 1}, []int{1, 7, 1}},
 	{[]int{1, 2, 3, 4, 4}, []int{}},
 	{[]int{5, 4, 4, 3, 3, 3}, []int{3}},
 	{[]int{7, 7, 7, 7, 7}, []int{7, 7, 7}},
@@ -14,8 +15,8 @@ var stampTests = []struct {
 
 func TestStamp(t *testing.T) {
 	for _, tc := range stampTests {
-		if Equal(Stamp(tc.in), tc.out) {
-			t.Errorf("")
+		if actual := Stamp(tc.in); !Equal(actual, tc.out) {
+			t.Errorf("expect %v got %v", tc.out, actual)
 		}
 	}
 }
