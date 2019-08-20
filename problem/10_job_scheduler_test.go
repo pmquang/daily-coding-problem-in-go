d2dd7beaeb2c0bc457563371ae8f45d4c3274dcf
khoi <khoiracle@gmail.com>
2019-08-20 17:05:20 +0700

    Fix race condition in test

--- problem/10_job_scheduler_test.go
+++ problem/10_job_scheduler_test.go
@@ -8,23 +8,26 @@ import (
 )
 
 func TestSchedule(t *testing.T) {
+	sliceMux := &sync.Mutex{}
 	var results []int
-	var expected []int
 	wg := &sync.WaitGroup{}
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 
 		work := func(index int) func() {
 			return func() {
+				sliceMux.Lock()
 				results = append(results, index)
+				sliceMux.Unlock()
 				wg.Done()
 			}
 		}
 
 		Schedule(work(i), i*100)
-		expected = append(expected, i)
 	}
 	wg.Wait()
+
+	expected := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	if !helper.Equal(expected, results) {
 		t.Errorf("expected %v, got %v", expected, results)
