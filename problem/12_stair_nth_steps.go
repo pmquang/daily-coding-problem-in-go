04c0d9f3a14b48cb6335390683120a1b7c1c6b6f
khoi <khoiracle@gmail.com>
2019-08-23 17:27:51 +0700

    Add memorization

--- problem/12_stair_nth_steps.go
+++ problem/12_stair_nth_steps.go
@@ -1,19 +1,20 @@
 package problem
 
 func CountStairSteps(n int, steps []int) int {
-	if n < 0 {
-		return 0
+	cache := make([]int, n+1)
+	cache[0] = 1
+
+	for i := 1; i < n; i++ {
+		cache[i] = 0
 	}
 
-	if n == 0 {
-		return 1
+	for i := 1; i <= n; i++ {
+		for _, step := range steps {
+			if i-step >= 0 {
+				cache[i] += cache[i-step]
+			}
+		}
 	}
 
-	count := 0
-
-	for _, step := range steps {
-		count += CountStairSteps(n-step, steps)
-	}
-
-	return count
+	return cache[n]
 }
