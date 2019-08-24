f056ea23fa5fe1d2d0e88cf2ed0ae5b50a9d14a3
khoi <khoiracle@gmail.com>
2019-08-24 17:39:50 +0700

    Add problem #13 - WIP

---
+++ problem/13_longest_substring_k_distinct_chars_test.go
@@ -0,0 +1,15 @@
+package problem
+
+import "testing"
+
+var longestSubstrTests = []struct {
+	s   string
+	k   int
+	out string
+}{
+	{"abcba", 2, "bcb"},
+}
+
+func TestCountLongestSubStr(t *testing.T) {
+
+}
